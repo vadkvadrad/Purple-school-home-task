cfbefffe1232d912572fb17c1c310bcc07c754eb
vadkvadrad <vadik.kudrv@yandex.ru>
2025-02-07 23:51:57 +0300

    обновлена модель user

--- 3-validation-api/internal/user/model.go
+++ 3-validation-api/internal/user/model.go
@@ -1,10 +1,30 @@
 package user
 
-import "gorm.io/gorm"
+import (
+	"math/rand"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
 	Email    string `json:"email"`
 	Password string `json:"password"`
 	Address  string `json:"address"`
+	Hash string `json:"hash"`
+	Valid bool `json:"valid"`
+}
+
+func GenerateHash() string {
+	return RandStringRunes(6)
+}
+
+var lettersRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range n {
+		b[i] = lettersRunes[rand.Intn(len(lettersRunes))]
+	}
+	return string(b)
 }
