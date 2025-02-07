cd928997a3d486a24beec007e28deff6c1e3d77e
vadkvadrad <vadik.kudrv@yandex.ru>
2025-02-07 20:48:08 +0300

    Создал config

---
+++ 3-validation-api/configs/config.go
@@ -0,0 +1,55 @@
+package configs
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Db DbConfig
+}
+
+type DbConfig struct {
+	Dsn string
+}
+
+func Load() *Config {
+	err := godotenv.Load(dir(".env"))
+	if err != nil {
+		log.Println("Error loading .env file, using default config.", "Error:", err.Error())
+	}
+
+	return &Config{
+		Db: DbConfig{
+			Dsn: os.Getenv("DSN"),
+		},
+	}
+
+}
+
+
+func dir(envFile string) string {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		goModPath := filepath.Join(currentDir, "go.mod")
+		if _, err := os.Stat(goModPath); err == nil {
+			break
+		}
+
+		parent := filepath.Dir(currentDir)
+		if parent == currentDir {
+			panic(fmt.Errorf("go.mod not found"))
+		}
+		currentDir = parent
+	}
+
+	return filepath.Join(currentDir, envFile)
+}
--- 3-validation-api/internal/email/handler.go
+++ 3-validation-api/internal/email/handler.go
@@ -1,2 +1,11 @@
 package email
 
+import "verify-api/configs"
+
+type EmailHandler struct {
+	Config *configs.Config
+}
+
+type EmailHandlerDeps struct {
+	
+}
