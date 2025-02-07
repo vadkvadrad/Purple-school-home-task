66e229694c2b4fb96686609e9314209465d03520
vadkvadrad <vadik.kudrv@yandex.ru>
2025-02-07 23:25:35 +0300

    добавлен gorm.Model в user

--- 3-validation-api/internal/user/model.go
+++ 3-validation-api/internal/user/model.go
@@ -1,8 +1,10 @@
 package user
 
+import "gorm.io/gorm"
+
 type User struct {
-	// gorm.Model
-	Email string `json:"email"`
+	gorm.Model
+	Email    string `json:"email"`
 	Password string `json:"password"`
-	Address string `json:"address"`
+	Address  string `json:"address"`
 }
